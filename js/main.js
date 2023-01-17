f59c0cc9470ac5697aa01de7f46bec42eba14793
DevFoxie <hmerniz.dev@gmail.com>
2023-01-17 15:53:50 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
-const keys = [90, 69, 81, 83, 68, 87, 88, 67];
-const sounds = ['hihat', 'Kick', 'openhat', 'boom', 'ride', 'snare', 'tom', 'tink'];
-const pad = document.querySelector('.pad');
+const pads = document.querySelectorAll('.key');
+const sound = document.querySelectorAll('audio');
+
+pads.forEach(pad => {
+  pad.addEventListener('transitionend', removeTransition);
+});
+
+pads.forEach(pad => {
+  pad.addEventListener('click', playSound);
+});
+
+document.addEventListener('keydown', playSound);
+
+// Fonction pour jouer le son
+function playSound(e) {
+  let keyCode;
+  if (e.type === 'keydown') {
+    keyCode = e.keyCode;
+  } else {
+    keyCode = this.getAttribute('data-key');
+  }
+
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+
+  const pad = document.querySelector(`div[data-key="${keyCode}"]`);
+
+  if (!audio) return;
+
+  pad.classList.add('playing');
+
+  audio.currentTime = 0;
+
+  // Lecture de l'audio
+  audio.play();
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
