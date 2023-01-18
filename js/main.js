9d513afed7fbf663b4cc9aa50268ed11b9ea6148
DevFoxie <hmerniz.dev@gmail.com>
2023-01-18 11:32:14 +0100

    second commit

--- js/main.js
+++ js/main.js
@@ -38,3 +38,101 @@ function removeTransition(e) {
   if (e.propertyName !== 'transform') return;
   this.classList.remove('playing');
 }
+
+// Récupération de la vidéo HTML
+const video = document.querySelector('#bg-video');
+video.style.display = 'none';
+
+// Récupération du bouton HTML 
+const button = document.querySelector("#beat-box-button");
+button.addEventListener("click", beatBox);
+button.addEventListener("click", function () {
+  video.style.display = 'block';
+  video.requestFullscreen();
+  video.play();
+});
+
+// Fonction beatbox pour une automatisation de touches
+function beatBox() {
+
+  function simulateKey(keyCode) {
+    let event = new KeyboardEvent("keydown", {
+      bubbles: true,
+      cancelable: true,
+      keyCode: keyCode
+    });
+    document.dispatchEvent(event);
+  }
+
+  function playBoom() {
+    return new Promise((resolve) => {
+      simulateKey(83); // keycode pour la touche S
+      setTimeout(resolve, 150); // délai pour la promesse
+    });
+  }
+
+  function playTomTink() {
+    return new Promise((resolve) => {
+      simulateKey(88); // keycode pour la touche X
+      simulateKey(67); // keycode pour la touche C
+      setTimeout(resolve, 150); // délai pour la promesse
+    });
+  }
+  function playKick() {
+    return new Promise((resolve) => {
+      simulateKey(69); // keycode pour la touche E
+      setTimeout(resolve, 150); // délai pour la promesse
+    });
+  }
+  function playOpenhat() {
+    return new Promise((resolve) => {
+      simulateKey(81); // keycode pour la touche Q
+      setTimeout(resolve, 150); // délai pour la promesse
+    });
+  }
+
+  // Chaîne de promesses dans une fonction récursive (répété x3)
+  function repeatSequence(n) {
+    if (n === 0) {
+        return Promise.resolve();
+    }
+    return playBoom()
+        .then(() => playBoom())
+        .then(() => playBoom())
+        .then(() => playBoom())
+        .then(() => playBoom())
+        .then(() => playBoom())
+        .then(() => playTomTink())
+        .then(() => playTomTink())
+        .then(() => playTomTink())
+        .then(() => playTomTink())
+        .then(() => playTomTink())
+        .then(() => playBoom())
+        .then(() => playKick())
+        .then(() => playBoom())
+        .then(() => playKick())
+        .then(() => playBoom())
+        .then(() => playKick())
+        .then(() => playBoom())
+        .then(() => playKick())
+        .then(() => playBoom())
+        .then(() => playKick())
+        .then(() => playBoom())
+        .then(() => playKick())
+        .then(() => playBoom())
+        .then(() => playBoom())
+        .then(() => playBoom())
+        .then(() => playBoom())
+        .then(() => playBoom())
+        .then(() => playKick())
+        .then(() => playKick())
+        .then(() => playKick())
+        .then(() => playKick())
+        .then(() => playKick())
+        .then(() => playKick())
+        .then(() => playOpenhat())
+        .then(() => repeatSequence(n - 1));
+}
+repeatSequence(3);
+}
+
